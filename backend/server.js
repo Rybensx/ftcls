228c8474323951503607d545e1669e834e32967c
Rybensx <rybensx@gmail.com>
2024-07-03 03:59:07 -0600

    configuracion de pg4 frontend path2 regreso con la variable frontend path

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const path = require('path');
+const { Pool } = require('pg'); // Importar el cliente de PostgreSQL
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Configura tu base de datos y otros middlewares aquí
-// ...
+// Configurar la conexión a PostgreSQL
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
+});
 
-// Define la ruta del frontend
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to PostgreSQL:', err);
+    process.exit(1); // Salir si no se puede conectar a la base de datos
+  } else {
+    console.log('PostgreSQL connected...');
+  }
+});
+
+app.use(express.json());
+
+// Middleware para servir archivos estáticos desde el directorio 'ftcls/frontend/build'
 const frontendPath = path.join(__dirname, '../frontend/build');
 app.use(express.static(frontendPath));
 
-// Maneja todas las rutas con el archivo index.html del frontend
+// Ruta para servir el archivo 'index.html' en todas las demás rutas
 app.get('*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
-// Inicia el servidor
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
